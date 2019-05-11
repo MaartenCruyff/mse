8c5afdf843aaa1a07e437b31168312ddda8ac267
Maarten Cruyff <m.cruyff@uu.nl>
2019-05-11 12:28:15 +0200

    Add mse package

---
+++ R/functions.R
@@ -0,0 +1,225 @@
+#' Stepwise model search
+#'
+#' @description \code{model_search} performs a stepwise log-linear model search.
+#' @param x a data frame with the incomplete lists and, if present, covariates.
+#' For the specification of the variables in \code{x}, see Details.
+#' @param lists a vector with the column numbers of \code{x} with the lists.
+#' @param k_max a scalar restricting the \emph{k}-order interaction terms to \code{k-max}.
+#' @param year character string with the name of the variable in \code{x} representing the
+#' years of the observations, if present.
+#' @param degree_year a scalar denoting the polynomial degree of the variable \code{year},
+#' if present.
+#' @return Prints a table with the degrees of freedom, deviance, AIC, AICc, BIC and population
+#' size estimates of the fitted models. The AIC, AICc and BIC are scaled in such a way that
+#' there minimum values equal 0. As input argument for the function \code{\link{boot_mse}} a
+#' list with the following objects is returned:
+#' \item{models}{the table with the fit statistics of the fitted models.}
+#' \item{formulas}{the formulas of the fitted models.}
+#' \item{coefs}{the parameter estimates of the fitted models.}
+#' \item{fits}{a list with the fitted frequencies of the models in the search path.}
+#' \item{minima}{a vector with the respective unscaled minima of the AIC, AICc and BIC.}
+#' \item{d}{the data frame \code{x} in the form of a contingency table.}
+#' \item{d_year}{the data frame \code{d} with \code{year} (if present) coded as polynomial.}
+#' \item{obs}{a vector distinguishing between observations and structural zeros in \code{d}.}
+#' \item{lists}{a vector with the column numbers of \code{d} with the lists.}
+#' \item{year}{a scalar indicating the column number of the variable \code{year} in \code{d}.}
+#' @details \code{x} should include at least two lists, and the lists should be coded 1 if the
+#' observation is in that list, and 0 if it is not.
+#'
+#' \code{x} can be either in an \eqn{n x p} format, where \eqn{n} is the number of observed
+#' individuals and \eqn{p} the number of variables (lists plus covariates), or in an
+#' \eqn{m x p + 1} format, where \eqn{m} is the number of all possible combination of the variable
+#' levels, and an additional variable called \code{Freq} with the frequencies (including the
+#' structural zeros).
+#'
+#' @importFrom stats coef extractAIC formula glm poisson poly predict step update
+#' @export
+
+
+
+model_search <- function(x, lists = NULL, k_max = 1, year = NULL, degree_year = NULL){
+
+
+  if(is.null(lists))stop("unspecified argument `lists`")
+  if(length(lists) < 2)stop("`lists` should have at least two elements")
+
+  if ("Freq" %in% colnames(x)){
+    nr       <- which(colnames(x)=="Freq")
+    x[, -nr] <- lapply(x[, -nr], as.factor)
+    lev      <- sapply(x[, -nr], levels)
+    if(nrow(x) != prod(sapply(lev, length)))stop("`x` has incorrect number of rows")
+  } else {
+    x <- lapply(x, as.factor)
+    x <- as.data.frame(table(x))
+  }
+
+  lev <- sapply(x[, -ncol(x)], levels)
+
+  if(!(max(as.numeric(unlist(lev[lists]))) == 1 &
+       min(as.numeric(unlist(lev[lists]))) == 0))stop("the lists should all be coded 0 and 1")
+
+  d <- x
+
+  if (!is.null(year) & !is.null(degree_year)){
+
+    if(length(levels(x[, paste(year)])) <= degree_year){
+      stop("`degree_year` equals/exceeds the number of years")
+    }
+
+    x[, paste(year)] <- poly(as.numeric(x[, paste(year)]), degree=degree_year, simple = T)
+  }
+
+  n   <- sum(x$Freq)
+  obs <- ifelse(rowSums(x[, lists] == "1") == 0, 0, 1)
+  m0  <- glm(Freq ~ ., poisson, data=subset(x, obs==1))
+  mx  <- glm(paste("Freq ~ . ^", k_max + 1), poisson, data=subset(x, obs==1))
+
+  m_bic <- step(m0, scope = list(lower = formula(m0), upper = formula(mx)), k=log(n), trace=0)
+  m_aic <- step(m_bic, scope = list(lower = formula(m0), upper = formula(mx)), trace=0)
+  m_nxt <- step(m_aic, scope = list(lower = formula(m_aic), upper = formula(mx)), k=1, trace=0)
+
+  B         <- data.frame(m_bic$anova,
+                          AICc = m_bic$anova$AIC,
+                          BIC  = m_bic$anova$AIC,
+                          Nhat = rep(0,nrow(m_bic$anova)))
+  B$Nhat[1] <- sum(predict(m0, newdata = x, type="response"))
+  B$AIC[1]  <- extractAIC(m0)[2]
+  B$AICc[1] <- extractAIC(m0)[2] + 2*(length(coef(m0))^2 + length(coef(m0)))/(n - length(coef(m0)) -1)
+  f         <- formula(m0)
+  fx        <- list(f)
+  cx        <- list(coef(m0))
+  nx        <- list(predict(m0, newdata = x, type="response"))
+
+  if(nrow(B) > 1){
+    for (j in 2:nrow(B)){
+      f         <- update(f, paste("~.", B$Step[j]))
+      m         <- glm(f, poisson, data=subset(x, obs==1))
+      nxx       <- predict(m, newdata=x, type="response")
+      B$AIC[j]  <- extractAIC(m, k=2)[2]
+      B$AICc[j] <- extractAIC(m, k=2)[2] + 2*(length(m$coef)^2 + length(m$coef))/(n - length(m$coef) - 1)
+      B$Nhat[j] <- sum(nxx)
+      fx[[length(fx) + 1]] <- f
+      cx[[length(cx) + 1]] <- coef(m)
+      nx[[length(nx) + 1]] <- nxx
+    }
+  }
+  if (nrow(m_aic$anova) > 1){
+    A         <- data.frame(m_aic$anova, AICc = m_aic$anova$AIC, BIC = m_aic$anova$AIC, Nhat=rep(0,nrow(m_aic$anova)))[-1, ]
+    for (j in 1:nrow(A)){
+      f         <- update(f, paste("~.", A$Step[j]))
+      m         <- glm(f, poisson, data=subset(x, obs==1))
+      nxx       <- predict(m, newdata=x, type="response")
+      A$BIC[j]  <- extractAIC(m, k=log(n))[2]
+      A$AICc[j] <- extractAIC(m, k=2)[2] + 2*(length(m$coef)^2 + length(m$coef))/(n - length(m$coef) - 1)
+      A$Nhat[j] <- sum(nxx)
+      fx[[length(fx) + 1]] <- f
+      cx[[length(cx) + 1]] <- coef(m)
+      nx[[length(nx) + 1]] <- nxx
+    }
+    B <- rbind(B, A)
+  }
+  if (nrow(m_nxt$anova) > 1){
+    A         <- data.frame(m_nxt$anova, AICc = m_nxt$anova$AIC, BIC = m_nxt$anova$AIC, Nhat=rep(0,nrow(m_nxt$anova)))[-1, ]
+    for (j in 1:nrow(A)){
+      f         <- update(f, paste("~.", A$Step[j]))
+      m         <- glm(f, poisson, data=subset(x, obs==1))
+      nxx       <- predict(m, newdata=x, type="response")
+      A$AIC[j]  <- extractAIC(m, k=2)[2]
+      A$AICc[j] <- extractAIC(m, k=2)[2] + 2*(length(m$coef)^2 + length(m$coef))/(n - length(m$coef) - 1)
+      A$BIC[j]  <- extractAIC(m, k=log(n))[2]
+      A$Nhat[j] <- sum(nxx)
+      fx[[length(fx) + 1]] <- f
+      cx[[length(cx) + 1]] <- coef(m)
+      nx[[length(nx) + 1]] <- nxx
+    }
+    B <- rbind(B, A)
+
+  }
+
+  B[, -1]     <- round(B[, -1], 1)
+  min_aic     <- min(B$AIC)
+  min_aicc    <- min(B$AICc)
+  min_bic     <- min(B$BIC)
+  B$AIC       <- B$AIC - min_aic
+  B$BIC       <- B$BIC - min_bic
+  B$AICc      <- B$AICc - min_aicc
+  rownames(B) <- 1:nrow(B)
+
+  options(scipen=999)
+  print(B)
+
+  invisible(list(models     = B,
+                 formulas   = fx,
+                 coefs      = cx,
+                 fits       = nx,
+                 minima     = c(AIC = min_aic, AICc = min_aicc, BIC = min_bic),
+                 d          = d,
+                 d_year     = x,
+                 obs        = obs,
+                 lists      = lists,
+                 year       = year))
+}
+
+
+
+#' Estimation of 95\% bootstrap confidence intervals.
+#'
+#' @description \code{boot_mse} performs a parametric bootstrap for a selected model.
+#' @param object an object created with the function \code{\link{model_search}}.
+#' @param modelnr the number of the model in \code{object} that is to be bootstrapped.
+#' If left unspecified, the  model with the lowest BIC will be selected.
+#' @param rep the number of bootstrap replications.
+#' @param seed a scalar to set the random seed.
+#' @return A lists with tables and, if the variable \code{year} is specified, with plots.
+#' \item{tables}{table(s) with the number of observations, the population size estimate with
+#' the 95\% bootstrap confidence interval. If covariates are present, the tables are presented
+#' for the levels of the covariate and, with more than one covariae, for the combinations of
+#' the levels of pairs of covariates. If the variable \code{year} is specified, these
+#' tables are presented by year.}
+#' \item{plots}{plots of the population size estimates and their 95\% confidence intervals,
+#' split out by the levels of the covariates and with the variable \code{year} on the x-axis.
+#' Only available if the variable \code{year} is specified.}
+#' @import doParallel
+#' @import ggplot2
+#' @importFrom foreach %dopar%
+#' @importFrom iterators icount
+#' @importFrom stats quantile rmultinom xtabs
+#' @export
+
+
+boot_mse <- function(object, modelnr = NULL, rep = 1000, seed = 1){
+
+  if(is.null(modelnr)){
+    modelnr <- which.min(object$models$BIC)
+  }
+
+  cl <- parallel::makeCluster(parallel::detectCores()/2)
+
+  doParallel::registerDoParallel(cl)
+
+  RNGkind("L'Ecuyer-CMRG")
+
+  parallel::clusterSetRNGStream(cl = cl, iseed=seed)
+  boot_fitted <- foreach::foreach(icount(rep), .combine=cbind) %dopar% {
+    bootx(b0  = object$coefs[[modelnr]],
+          f   = object$formulas[[modelnr]],
+          N   = object$models$Nhat[[modelnr]],
+          f0  = object$fits[[modelnr]],
+          d   = object$d_year,
+          obs = object$obs)
+  }
+
+  parallel::stopCluster(cl)
+
+  invisible(
+    generate_tables(d     = object$d,
+                    lists = object$lists,
+                    m     = object$fits[[modelnr]],
+                    year  = object$year,
+                    boot_fitted = boot_fitted)
+  )
+
+}
+
+
+
---
+++ R/helpfunctions.R
@@ -0,0 +1,221 @@
+
+bootx <- function(b0, f, N, f0, d, obs){
+
+  d$Freq <- rmultinom(n=1, size=N, prob=f0/N)
+  mx     <- glm(f, poisson, data=subset(d, obs==1), start=b0)
+  freqs  <- predict(mx, newdata = d, type="response")
+
+  return(freqs)
+
+}
+
+ci_95 <- function(x){
+  v <- quantile(x, probs = c(0.025, 0.975))
+  names(v) <- c("min95", "max95")
+  v
+}
+
+
+gen_plot <- function(temp, ttl = names(est)[length(est)], gr = NULL){
+
+  limx <- c(min(temp[, 1]), max(temp[, 1]))
+  labx <- unique(temp[, 1])
+  if(is.null(gr)){
+    p <- ggplot2::ggplot(data.frame(temp), aes(x=Year, y=Nhat))+
+      geom_line() +
+      geom_ribbon(aes(x=Year, ymin=min95, ymax=max95), linetype=0, alpha=.2) +
+      ggtitle(ttl) +
+      ylim(0, NA) +
+      scale_x_continuous(name="year", breaks=labx, labels=labx) +
+      theme_light() +
+      theme(legend.title=element_blank())
+  }
+
+  if(!is.null(gr)){
+    p <- ggplot2::ggplot(data.frame(temp), aes(x=Year, y=Nhat, group = gr, col = gr))+
+      geom_line() +
+      geom_ribbon(aes(x=Year, ymin=min95, ymax=max95, fill = gr), linetype=0, alpha=.2) +
+      ggtitle(ttl) +
+      ylim(0, NA) +
+      scale_x_continuous(name="year", breaks=labx, labels=labx) +
+      theme_light() +
+      theme(legend.title=element_blank())
+  }
+
+  p
+}
+
+
+
+generate_tables <- function(d, lists, m, year, boot_fitted){
+
+  est       <- list()
+  est[[1]]  <- round(cbind(nobs = sum(d$Freq),
+                            Nhat = sum(m),
+                            t(ci_95(colSums(boot_fitted)))))
+  names(est)[[1]]    <- "x"
+  rownames(est[[1]]) <- "all"
+
+  if(!is.null(year)){
+
+    plots <- list()
+
+    yearnr  <- which(colnames(d) == year)
+    temp    <- matrix(0, length(levels(d[, yearnr])), 5,
+                      dimnames = list(levels(d[, yearnr]),
+                                      c("Year", "nobs", "Nhat", "min95", "max95")))
+
+    for(j in 1:length(levels(d[, yearnr]))){
+
+      nrs        <- which(d[, yearnr] == levels(d[, yearnr])[j])
+      temp[j, ]  <- round(cbind(Year = as.numeric(levels(d[, yearnr])[j]),
+                                nobs = sum(d$Freq[nrs]),
+                                Nhat = sum(m[nrs]),
+                                t(ci_95(colSums(boot_fitted[nrs, ])))))
+    }
+
+    est[[length(est) + 1]]    <- temp
+    plots[[1]]                <- gen_plot(temp = temp, ttl = names(est)[length(est)])
+    names(est)[[length(est)]] <- names(plots)[[1]] <- colnames(d)[yearnr]
+
+    }
+
+
+
+
+  if(ncol(d) > length(lists) + 1 & is.null(year)){    #no year and only one covariate
+
+    covs <- d[, -lists]
+    lev  <- lapply(covs[, -ncol(covs), drop = F], levels)
+
+    for(i in 1:length(lev)){
+      temp <- matrix(0, length(lev[[i]]), 4,
+                     dimnames = list(lev[[i]],
+                                     c("nobs", "Nhat", "min95", "max95")))
+      for(j in 1:length(lev[[i]])){
+        nrs <- which(covs[, i] == lev[[i]][j])
+        temp[j, ]  <- round(cbind(nobs = sum(covs$Freq[nrs]),
+                                  Nhat = sum(m[nrs]),
+                                  t(ci_95(colSums(boot_fitted[nrs, ])))))
+      }
+      est[[length(est) + 1]]     <- temp
+      names(est)[[length(est)]]  <- names(lev)[i]
+    }
+
+
+  }
+
+  if(ncol(d) > length(lists) + 2 & is.null(year)){     #no year and at least two covariates
+
+
+      for(i in 1:(length(lev) - 1)){
+        for(j in (i+1):length(lev)){
+
+          z         <- xtabs(boot_fitted ~ ., covs[, c(i, j)])
+          temp      <- NULL
+          tempnames <- NULL
+
+          for(k in 1:length(lev[[i]])){
+            for(l in 1:length(lev[[j]])){
+
+              nrs <- which(covs[, i] == lev[[i]][k] & covs[, j] == lev[[j]][l])
+
+              vtemp <- round(cbind(nobs = sum(covs$Freq[nrs]),
+                                   Nhat = sum(m[nrs]),
+                                   t(ci_95(colSums(boot_fitted[nrs, ])))))
+              temp <- rbind(temp, vtemp)
+              tempnames <- c(tempnames, paste(lev[[i]][k], lev[[j]][l], sep = ":"))
+
+            }
+          }
+          rownames(temp)            <- tempnames
+          est[[length(est) + 1]]    <- temp
+          names(est)[[length(est)]] <-  paste(names(lev)[i], names(lev)[j], sep = "x")
+        }
+      }
+    }
+
+  if(ncol(d) > length(lists) + 2 & !is.null(year)){       #year and at leat one covariate
+
+    covs    <- d[, -lists]
+    lev     <- lapply(covs[, -ncol(covs), drop = F], levels)
+
+    yearnr  <- which(colnames(covs) == year)
+    covnr   <- (1:ncol(covs))[-c(yearnr, ncol(covs))]
+
+    temp   <- matrix(0, length(lev[[yearnr]]), 5,
+                   dimnames = list(lev[[yearnr]],
+                                   c("Year", "nobs", "Nhat", "min95", "max95")))
+
+
+    for(i in covnr){
+
+      temp      <- NULL
+      tempnames <- NULL
+
+      for(j in 1:length(lev[[i]])){
+        for(k in 1:length(lev[[yearnr]])){
+          nrs        <- which(covs[, i] == lev[[i]][j]  & covs[, yearnr] == lev[[yearnr]][k])
+          vtemp      <- round(cbind(Year = as.numeric(lev[[yearnr]][k]),
+                                    nobs = sum(covs$Freq[nrs]),
+                                    Nhat = sum(m[nrs]),
+                                    t(ci_95(colSums(boot_fitted[nrs, ])))))
+          temp       <- rbind(temp, vtemp)
+          tempnames  <- c(tempnames, paste(lev[[i]][j]))
+        }
+      }
+      rownames(temp)                <- tempnames
+      est[[length(est) + 1]]        <- temp
+      names(est)[[length(est)]]     <- names(lev)[i]
+      plots[[length(plots) + 1]]    <- gen_plot(temp = temp, ttl = names(est)[length(est)], gr = as.factor(rownames(temp)))
+      names(plots)[[length(plots)]] <- names(lev)[i]
+    }
+  }
+
+  if(ncol(d) > length(lists) + 3 & !is.null(year)){       #year and at leat two covariates
+
+    yearnr  <- which(colnames(covs) == year)
+    covnr   <- (1:ncol(covs))[-c(yearnr, ncol(covs))]
+
+
+    for(i in covnr[-length(covnr)]){
+      for(j in (i + 1):length(covnr)){
+
+        temp      <- NULL
+        tempnames <- NULL
+
+        for(k in 1:length(lev[[i]])){
+          for(l in 1:length(lev[[j]])){
+            for(y in 1:length(lev[[yearnr]])){
+
+              nrs <- which(covs[, i] == lev[[i]][k] & covs[, j] == lev[[j]][l] & covs[, yearnr]  == lev[[yearnr]][y])
+
+              vtemp <- round(cbind(Year = as.numeric(lev[[yearnr]][y]),
+                                   nobs = sum(covs$Freq[nrs]),
+                                   Nhat = sum(m[nrs]),
+                                   t(ci_95(colSums(boot_fitted[nrs, ])))))
+              temp      <- rbind(temp, vtemp)
+              tempnames <- c(tempnames, paste(lev[[i]][k], lev[[j]][l], sep = ":"))
+            }
+          }
+        }
+        rownames(temp)                <- tempnames
+        est[[length(est) + 1]]        <- temp
+        names(est)[[length(est)]]     <- paste(names(lev)[i], names(lev)[j], sep = "x")
+        plots[[length(plots) + 1]]    <- gen_plot(temp = temp, ttl = names(est)[length(est)], gr = as.factor(rownames(temp)))
+        names(plots)[[length(plots)]] <- paste(names(lev)[i], names(lev)[j], sep = "x")
+      }
+    }
+  }
+
+  if(is.null(year)){
+
+    return(est)
+
+  }else{
+
+    return(list(tables = est,
+                plots = plots))
+  }
+
+}
